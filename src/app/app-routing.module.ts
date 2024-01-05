d68fb333b6547befa642783d4d29030298fa9165
RedOuaneAO <redouaneao97@gmail.com>
2024-01-05 12:24:03 +0100

    create project stractur and install ngrx store and effects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskComponent } from './task/component/task/task.component';
+import { TagComponent } from './tag/component/tag/tag.component';
+import { UserComponent } from './user/component/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'tasks' , component:TaskComponent},
+  {path:'tags' , component:TagComponent},
+  {path:'users' , component:UserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { UserComponent } from './user/component/user/user.component';
+import { AddUserComponent } from './user/component/add-user/add-user.component';
+import { TaskComponent } from './task/component/task/task.component';
+import { TagComponent } from './tag/component/tag/tag.component';
+import { AddTaskComponent } from './task/component/add-task/add-task.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './sharedComponent/component/nav-bar/nav-bar.component';
+import { SideBarComponent } from './sharedComponent/component/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AddUserComponent,
+    TaskComponent,
+    TagComponent,
+    AddTaskComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
